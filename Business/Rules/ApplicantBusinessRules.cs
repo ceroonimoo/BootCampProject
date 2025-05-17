f2ce4b848e2455e418f78913802831df102eb5ce
ceroonimoo <varkal3ceren@gmail.com>
2025-05-18 00:03:07 +0300

    .

---
+++ Business/Rules/ApplicantBusinessRules.cs
@@ -0,0 +1,28 @@
+﻿using Core.Exceptions.Types;
+using Core.Rules;
+using Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Rules
+{
+    public class ApplicantBusinessRules : BaseBusinessRules
+    {
+        private readonly IApplicantRepository _applicantRepository;
+
+        public ApplicantBusinessRules(IApplicantRepository applicantRepository)
+        {
+            _applicantRepository = applicantRepository;
+        }
+
+        public void CheckIfTCNoIsUnique(string TC)
+        {
+            var existingApplicant = _applicantRepository.Get(b => b.Name == name);
+            if (existingApplicant != null)
+                throw new BusinessException($"{TC} Aynı tc numaralı biri mevcut");
+        }
+    }
+}
