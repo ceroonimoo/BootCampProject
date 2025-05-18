b443ce5bebbb9bea04610ee44c2ac71c93a51de7
Yunus <yunus.cakir@protonmail.com>
2025-05-18 11:57:18 +0300

    Commit

---
+++ Business/Rules/ BootcampBusinessRules.cs
@@ -0,0 +1,30 @@
+﻿using Core.Exceptions.Types;
+using Core.Rules;
+using Entities.Enums;
+using Repositories;
+using System;
+
+namespace Business.Rules
+{
+    public class BootcampBusinessRules : BaseBusinessRules
+    {
+        private readonly IBootcampRepository _bootcampRepository;
+        private readonly IInstructorRepository _instructorRepository;
+
+        public BootcampBusinessRules(
+            IBootcampRepository bootcampRepository,
+            IInstructorRepository instructorRepository)
+        {
+            _bootcampRepository = bootcampRepository;
+            _instructorRepository = instructorRepository;
+        }
+
+        public void CheckIfStartDateBeforeEndDate(DateTime startDate, DateTime endDate)
+        {
+            if (startDate >= endDate)
+                throw new BusinessException("Bootcamp başlangıç tarihi, bitiş tarihinden önce olmalıdır.");
+        }
+
+        public void CheckIfBootcampNameIsUnique(string name)
+        {
+            var existingBootcamp = _bootcampRepository.Get(b => b.Name == _
